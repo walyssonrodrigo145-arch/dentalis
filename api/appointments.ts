852aee28698d2c26f53edcd64756e25b5afdd992
walyssonrodrigo145-arch <walyssonrodrigo145@gmail.com>
2026-05-19 14:03:34 -0300

    fix: append .js extension to relative import in api/ endpoints for ESM Node16/NodeNext compatibility

--- api/appointments.ts
+++ api/appointments.ts
@@ -1,4 +1,4 @@
-import { initDatabase, pool } from "./_db"
+import { initDatabase, pool } from "./_db.js"
 
 export default async function handler(req: any, res: any) {
   res.setHeader("Access-Control-Allow-Origin", "*")
